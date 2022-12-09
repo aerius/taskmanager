306e1b631283912a5b4f9a987f56c7212caf2a20
Bert Scholten <bscholten@dat.nl>
2022-12-09 12:23:15 +0100

    Implemented opentelemetry gauges for workerpool size/use
    worker_size and current_worker_size are pretty similar, though there can be cases where they are different (when workers drop down but there are still tasks being handled the current_worker_size can be temporarily higher),

--- source/taskmanager/src/main/java/nl/aerius/taskmanager/WorkerPoolMetrics.java
+++ source/taskmanager/src/main/java/nl/aerius/taskmanager/WorkerPoolMetrics.java
@@ -16,22 +16,86 @@
  */
 package nl.aerius.taskmanager;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.ObservableDoubleGauge;
+
 /**
  * Set up metric collection for this worker pool with the given type name.
  */
 public final class WorkerPoolMetrics {
+
+  private static final Meter METER = GlobalOpenTelemetry.getMeter("nl.aerius.taskmanager");
+
+  private static final Map<String, ObservableDoubleGauge> REGISTERED_METRICS = new HashMap<>();
+
+  private enum WorkerPoolMetricType {
+    // @formatter:off
+    WORKER_SIZE(WorkerPool::getWorkerSize, "Configured number of workers according to taskmanager"),
+    CURRENT_WORKER_SIZE(WorkerPool::getCurrentWorkerSize, "Current number of workers according to taskmanager"),
+    RUNNING_WORKER_SIZE(WorkerPool::getRunningWorkerSize, "Running (or occupied) number of workers according to taskmanager");
+    // @formatter:on
+
+    private final Function<WorkerPool, Integer> function;
+    private final String description;
+
+    private WorkerPoolMetricType(final Function<WorkerPool, Integer> function, final String description) {
+      this.function = function;
+      this.description = description;
+    }
+
+    int getValue(final WorkerPool workerPool) {
+      return function.apply(workerPool);
+    }
+
+    String getGaugeName() {
+      return "aer.taskmanager." + name().toLowerCase();
+    }
+
+    String getDescription() {
+      return description;
+    }
+
+  }
+
   private WorkerPoolMetrics() {
   }
 
   public static void setupMetrics(final WorkerPool workerPool, final String workerQueueName) {
-    // TODO use OpenTelemetry to register the following as gauge.
-    // workerPool.getWorkerSize()
-    // workerPool.getCurrentWorkerSize()
-    // workerPool.getRunningWorkerSize()
+    final Attributes attributes = Attributes.of(AttributeKey.stringKey("worker_type"), workerIdentifier(workerQueueName));
+    for (final WorkerPoolMetricType metricType : WorkerPoolMetricType.values()) {
+      REGISTERED_METRICS.put(gaugeIdentifier(workerQueueName, metricType),
+          METER.gaugeBuilder(metricType.getGaugeName())
+              .setDescription(metricType.getDescription())
+              .buildWithCallback(
+                  result -> result.record(metricType.getValue(workerPool), attributes)));
+    }
   }
 
   public static void removeMetrics(final String workerQueueName) {
-    // TODO unregister OpenTelemetry gauges (if possible)
+    for (final WorkerPoolMetricType metricType : WorkerPoolMetricType.values()) {
+      final String gaugeId = gaugeIdentifier(workerQueueName, metricType);
+      if (REGISTERED_METRICS.containsKey(gaugeId)) {
+        REGISTERED_METRICS.remove(gaugeId).close();
+      }
+    }
+  }
+
+  private static String workerIdentifier(final String workerQueueName) {
+    final int workerTypeIndex = workerQueueName.lastIndexOf('.');
+
+    return (workerTypeIndex > 0 ? workerQueueName.substring(workerTypeIndex) : workerQueueName).toUpperCase(Locale.ROOT);
+  }
+
+  private static String gaugeIdentifier(final String workerQueueName, final WorkerPoolMetricType gaugeType) {
+    return workerQueueName + "_" + gaugeType.name();
   }
 
 }
