753cad939d35ebef724659b5780e5f0abf232a86
Hilbrand Bouwkamp <hilbrand@h72.nl>
2026-05-22 13:50:05 +0200

    AER-4462 TaskManager should restart channels to the worker when shutdown (not by application)
    This can happen when connection to RabbitMQ is lost, for example when RabbitMQ is restarted.

--- source/taskmanager/src/test/java/nl/aerius/taskmanager/mq/RabbitMQWorkerProducerTest.java
+++ source/taskmanager/src/test/java/nl/aerius/taskmanager/mq/RabbitMQWorkerProducerTest.java
@@ -17,6 +17,9 @@
 package nl.aerius.taskmanager.mq;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
 import java.util.concurrent.Semaphore;
@@ -27,8 +30,10 @@ import org.junit.jupiter.api.Timeout;
 import org.mockito.Mock;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
 
 import nl.aerius.taskmanager.adaptor.WorkerProducer;
 import nl.aerius.taskmanager.adaptor.WorkerSizeObserver;
@@ -48,7 +53,7 @@ class RabbitMQWorkerProducerTest extends AbstractRabbitMQTest {
   void testForwardMessage() throws IOException, InterruptedException {
     final byte[] sendBody = "4321".getBytes();
 
-    final WorkerProducer wp = adapterFactory.createWorkerProducer(new QueueConfig(WORKER_QUEUE_NAME, false, false, -1, null));
+    final WorkerProducer wp = createWorkerProducer();
     wp.start();
     final BasicProperties bp = new BasicProperties();
     wp.dispatchMessage(new RabbitMQMessage(WORKER_QUEUE_NAME, null, 4321, bp, sendBody) {
@@ -69,5 +74,40 @@ class RabbitMQWorkerProducerTest extends AbstractRabbitMQTest {
     });
     lock.tryAcquire(1, 5, TimeUnit.SECONDS);
     assertArrayEquals(sendBody, data.getData(), "Test if body send");
+  }
+
+  @Test
+  @Timeout(value = 5, unit = TimeUnit.SECONDS)
+  void testRestart() throws IOException {
+    final Connection connection = factory.getConnection();
+    final WorkerProducer wp = createWorkerProducer();
+
+    if (wp instanceof final RabbitMQWorkerProducer rwp) {
+      // First start which should create the channels.
+      wp.start();
+      verify(connection, times(1)).createChannel(); // worker channel
+      verify(connection, times(1)).openChannel();   // worker reply channel
+      // Second call if shutdown was done, but not by application so channels should be recreated.
+      assertShutdownCreatesNewChannelsCorrectly(rwp, false);
+      // Third call if shutdown was done by application, channels should not be recreated.
+      assertShutdownCreatesNewChannelsCorrectly(rwp, true);
+      // Forth call shutdown, after that it should also not create new channels.
+      wp.shutdown();
+      assertShutdownCreatesNewChannelsCorrectly(rwp, false);
+    } else {
+      fail("Expected worker producer to be of type RabbitMQWorkerProducer, but was: " + wp.getClass());
+    }
+  }
+
+  private void assertShutdownCreatesNewChannelsCorrectly(final RabbitMQWorkerProducer wp, final boolean initiatedByApplication) throws IOException {
+    final Connection connection = factory.getConnection();
+
+    wp.shutdownCompleted(new ShutdownSignalException(false, initiatedByApplication, null, wp));
+    verify(connection, times(2)).createChannel();
+    verify(connection, times(2)).openChannel();
+  }
+
+  private WorkerProducer createWorkerProducer() {
+    return adapterFactory.createWorkerProducer(new QueueConfig(WORKER_QUEUE_NAME, false, false, -1, null));
   }
 }
